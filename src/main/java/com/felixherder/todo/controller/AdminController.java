bcd9b90c0c158a58880331abe0989011958f6263
Vass Paul <vass_paul95@yahoo.com>
2025-03-27 21:01:41 +0200

    updates

--- src/main/java/com/felixherder/todo/controller/AdminController.java
+++ src/main/java/com/felixherder/todo/controller/AdminController.java
@@ -1,11 +1,17 @@
 package com.felixherder.todo.controller;
 
+import com.felixherder.todo.model.Todo;
+import com.felixherder.todo.model.User;
+import com.felixherder.todo.service.AdminService;
 import com.felixherder.todo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin")
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     private final UserService userService;
+    private final AdminService adminService;
 
     @GetMapping("/viewUsers")
     public String getUsers(Model model){
@@ -20,5 +27,18 @@ public class AdminController {
         return "usersList";
     }
 
+    @GetMapping("/createTask")
+    public String getCreateTaskPage(Model model, @RequestParam Integer userId) {
+        model.addAttribute("userId", userId);
+        model.addAttribute("todo", new Todo());
 
+        return "createTask";
+    }
+
+    @PostMapping
+    public String createTask(@ModelAttribute Todo todo, @RequestParam Integer userId) {
+        User user = userService.findById(userId);
+        adminService.addTaskToUser(user, todo);
+        return "redirect:/admin/viewUsers";
+    }
 }
--- src/main/java/com/felixherder/todo/service/AdminService.java
+++ src/main/java/com/felixherder/todo/service/AdminService.java
@@ -1,25 +1,21 @@
 package com.felixherder.todo.service;
 
+import com.felixherder.todo.model.Todo;
 import com.felixherder.todo.model.User;
 import com.felixherder.todo.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class AdminService {
 
     private final UserRepo userRepo;
 
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
-    }
 
-    public User findById(Integer id) {
-        return userRepo.findById(id).orElseThrow(() -> new RuntimeException("no user found"));
+    public void addTaskToUser(User user, Todo todo) {
+        String userName = user.getUsername();
+        userRepo.findByUsername(userName).getTodoList().add(todo);
     }
 
 }
